52041105aae719d079d7ab02d90e9bcbb4331ee9
crodrigm3 <crodrigm3@gmail.com>
2022-10-20 16:02:34 -0500

    Crear carpetas y conectar servidor y base de datos

---
+++ back/config/database.js
@@ -0,0 +1,17 @@
+//
+const mongoose = require("mongoose");
+
+//Método de conexion a mongo DB
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,  //Permite q entre una nueva url
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`Base de datos mongo conectada con el servidor: ${con.connection.host}`)
+    }).catch(con => {
+        console.log(`No se logro la conexion con la BD`)
+    })
+}
+
+//Exportamos el metodo: connectDatabase
+module.exports = connectDatabase;
