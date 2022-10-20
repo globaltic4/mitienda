dd8b634993f1a53c8330b89fde3b1e58bf15b853
Camilo <czlombana@hotmail.com>
2022-10-20 16:02:47 -0500

    Crear carpetas y configurar Servidor y BD

---
+++ back/app.js
@@ -0,0 +1,13 @@
+const express = require("express");  //Intercomunicador entre el back y el front
+const app = express();
+
+app.use(express.json());
+
+/*Importar las rutas
+const productos = require("./routes/products")
+
+//Creamos la ruta del navegador
+app.use('/api', productos)
+*/
+//Exportamos la 'app'
+module.exports = app;
