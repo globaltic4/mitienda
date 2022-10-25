54db539d660e85d8d4b289f221ba90edbcaa5a37
Camilo <czlombana@hotmail.com>
2022-10-25 17:37:27 -0500

    Sprint2, Interface: Lista de productos [Admin, Client]

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layaout/Header';
+import { Footer } from './components/layaout/Footer';
+import Home from './components/Home';
+
+//Router traido desde react-router-dom (no confundir con el de express)
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Global TIC mi tienda de celulares</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className='contenidoPrincipal'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
