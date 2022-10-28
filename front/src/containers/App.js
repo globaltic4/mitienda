2fda8506920d653c5f961ca08a60346414f8937e
Camilo <czlombana@hotmail.com>
2022-10-27 20:41:49 -0500

    Sprint 2, Actualizar Carpetas y mover archivos

--- front/src/containers/App.js
+++ front/src/containers/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
-import Header from './components/layaout/Header';
-import { Footer } from './components/layaout/Footer';
-import Home from './components/Home';
+import Header from '../components/layaout/Header';
+import { Footer } from '../components/layaout/Footer';
+import Home from '../components/Home';
 
 //Router traido desde react-router-dom (no confundir con el de express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
