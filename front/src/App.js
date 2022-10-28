6f27eb51cac1774b93b6a03e4f68b4806622a48c
crodrigm3 <crodrigm3@gmail.com>
2022-10-27 19:02:26 -0500

    cambiar className

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Global TIC 4</h1>
-    </div>
-=======
-import './App.css';
-import React from 'react';
-import Header from './components/layaout/Header';
-import { Footer } from './components/layaout/Footer';
+import Header from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 import Home from './components/Home';
 
 //Router traido desde react-router-dom (no confundir con el de express)
@@ -32,7 +21,6 @@ function App() {
         <Footer />
       </div>
     </Router>
->>>>>>> 54db539d660e85d8d4b289f221ba90edbcaa5a37
   );
 }
 
--- front/src/index.js
+++ front/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './App';
-=======
-import App from './App.js';
->>>>>>> 54db539d660e85d8d4b289f221ba90edbcaa5a37
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 54db539d660e85d8d4b289f221ba90edbcaa5a37
